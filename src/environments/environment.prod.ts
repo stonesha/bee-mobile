f86de268ed9c8b71b349b06a983cb32623280164
stonesha <stoneshaus@gmail.com>
2020-12-28 22:36:51 -0800

    ionic env variables need money so commiting api key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+
+  mapbox: {
+    accessToken: 'pk.eyJ1Ijoic3RvbmVzaGEiLCJhIjoiY2tneXlmeHBwMGhwbzJwb3g2MjNiN3MyYSJ9.nhINfN-bW0q7OMoHHvibew'
+  }
 };
