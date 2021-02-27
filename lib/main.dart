79bd37915ea99147d3d47179e0c633162bc20cd0
Stone Sha <36431006+stonesha@users.noreply.github.com>
2021-02-27 12:34:07 -0800

    aquireCurrentLocation() added

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,44 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:location/location.dart';
+
+Future<LatLng> acquireCurrentLocation() async {
+  //initilaizes plugin and starts listening for potential platform events
+  Location location = new Location();
+
+  //whether or not the locationservice is enabled
+  bool serviceEnabled;
+
+  //status of a permission request to use location services
+  PermissionStatus permissionGranted;
+
+  //check if the location service is enabled and if not request it
+  //if user refuses, return null
+  serviceEnabled = await location.serviceEnabled();
+  if (!serviceEnabled) {
+    serviceEnabled = await location.requestService();
+
+    if (!serviceEnabled) {
+      return null;
+    }
+  }
+
+  //check location permissions, similar in android apps
+  //check if permissions granted, if not first request,
+  //then read result, only proceed if permission is granted
+  permissionGranted = await location.hasPermission();
+  if (permissionGranted == PermissionStatus.denied) {
+    permissionGranted = await location.requestPermission();
+    if (permissionGranted != PermissionStatus.granted) {
+      return null;
+    }
+  }
+
+  final locationData = await location.getLocation();
+
+  return LatLng(locationData.latitude, locationData.longitude);
+}
 
 void main() {
   runApp(MyApp());
